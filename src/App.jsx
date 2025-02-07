78754c4b438459e3ff7464d2354ca7ba12844ff7
kevin Hernandez <smsmaintex01@gmail.com>
2025-02-07 00:51:56 -0400

    First Commit: React/vite and Tailwind configured

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HeroComponent from './sections/hero'
+import NavbarComponent from './sections/navbar'
+
+function App() {
+
+  return (
+    <div className=''>
+      <NavbarComponent />
+      <HeroComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/sections/navbar/index.jsx
@@ -0,0 +1,33 @@
+import "./navbar.css";
+
+const NavbarComponent = () => {
+  return (
+
+  <></>
+    // <nav className="navbar top-0 sticky z-50">
+    //   <div className="bg-gray-200 px-4 py-4">
+    //     <div className="w-full md: ml-10 md:max-w-6xl md:mx-auto md:flex md:items-center md:justify-between">
+    //       <div>
+    //         <a
+    //           href="#"
+    //           className="inline-block py-2 text-gray-800 text-4xl font-bold"
+    //         >
+    //           Qiknest
+    //         </a>
+    //       </div>
+
+    //       <div className="hidden md:block">
+    //         <a
+    //           href="#"
+    //           className="inline-block py-2 px-4 text-gray-700 bg-white hover:bg-gray-100 rounded-lg"
+    //         >
+    //           Únete a nuestro newsletter
+    //         </a>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </nav>
+  );
+};
+
+export default NavbarComponent;
